f81a551819ec7725b9f0e7b15b8b4d6b9538d947
Kirsty Hunter <kirst2k@hotmail.com>
2015-12-14 21:12:50 +0000

    Two different books returns discounted price

--- PotterBooksKata/Tests.cs
+++ PotterBooksKata/Tests.cs
@@ -13,33 +13,43 @@ namespace PotterBooksKata
         [Test]
         public void Buying_SingleBook_ShouldReturn_8Euro()
         {
+            var book = new int[]{ 1 };
             var basket = new ShoppingBasket();
-            var total = basket.Total(1);
+            var total = basket.Total(book);
             Assert.AreEqual(8, total);
         }
 
         [Test]
         public void Buying_TwoofTheSameBook_ShouldReturn_16Euro()
         {
+            var book = new int[] { 1, 1 };
             var basket = new ShoppingBasket();
-            var total = basket.Total(2);
+            var total = basket.Total(book);
             Assert.AreEqual(16, total);
         }
 
         [Test]
         public void buying_TwoDifferentBooks_ShouldReturnDiscountedPrice()
         {
+            var expected = 16 - (8 * 2 * 5 / 100);
+            var book = new int[] { 1, 2 };
             var basket = new ShoppingBasket();
-            var total = basket.Total(2);
-            Assert.AreEqual(15.20, total);
+            var total = basket.Total(book);
+            Assert.AreEqual(expected, total);
         }
     }
 
     public class ShoppingBasket
     {
-        public int Total(int bookCount)
+        public decimal Total(int[] books)
         {
-            return 8 * bookCount;
+            var bookCount = books.Count();
+            var total = 8 * bookCount;
+            if(books.Distinct().Count() == 2)
+            {
+                return total - (8 * books.Distinct().Count() * 5 / 100);
+            }
+            return total;
         }
     }
 }
